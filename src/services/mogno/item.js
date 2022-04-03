7ce9aec9480c5c0f3593ec3c969a8eb156a94bee
Hassan <hassan@gmail.com>
2022-04-03 09:07:18 +0500

    item-service: code refactor i.e. remove unused code and comments

--- src/services/mogno/item.js
+++ src/services/mogno/item.js
@@ -8,32 +8,6 @@ async function filteredItems(filterOptions) {
   console.log("filter ops: ", filterOptions);
   const { isOutOfStock, priceRange, sort, searchVal } = filterOptions;
 
-  let excludeStockQuery = "";
-  if (isOutOfStock) {
-    excludeStockQuery = " and quantity <> 0";
-  }
-
-  let sortQuery = "";
-  if (sort) {
-    let { sortOrder, sortKey } = sort;
-    sortOrder = sortOrder ?? "asc";
-    if (sortOrder) {
-      sortQuery = " order by " + sortKey + " " + sortOrder;
-    }
-  }
-
-  let priceRangeQuery = "";
-  if (priceRange) {
-    const { min, max } = priceRange;
-    if (!max || max > 0) {
-      priceRangeQuery = `and (price >= ${min === 0 ? 1 : min})`;
-    } else {
-      priceRangeQuery = ` and  (price between ${
-        min === 0 ? 1 : min
-      } and ${max})`;
-    }
-  }
-
   const nameQuery = { name: { $regex: ".*" + searchVal + ".*" } };
   let priceQuery = {};
   if (priceRange) {
@@ -49,8 +23,6 @@ async function filteredItems(filterOptions) {
     stockQuery = { quantity: { $ne: 0 } };
   }
 
-  console.log("pricequery: ", priceQuery);
-  console.log("stockQuery: ", stockQuery);
   const filteredItems = db.Item.find({
     ...nameQuery,
     ...priceQuery,
@@ -65,29 +37,9 @@ async function filteredItems(filterOptions) {
   }
 
   return filteredItems;
-
-  return await db.sequelize.query(
-    `
-    SELECT 
-      id,
-      name,
-      price,
-      quantity,
-      featured_image
-    FROM items
-    WHERE name like '%${searchVal}%'
-    ${priceRangeQuery}
-    ${excludeStockQuery}
-    ${sortQuery}
-  `,
-    { type: QueryTypes.SELECT }
-  );
 }
 
 async function singleItem(itemId) {
-  //   return db.Item.findByPk(itemId, {
-  //     include: [{ model: db.Shop }, { model: db.ItemImage }],
-  //   });
   return db.Item.findById(itemId).populate("shopId");
 }
 
@@ -96,9 +48,6 @@ async function createNewitem(itemFields) {
     let newItem = new db.Item({ ...itemFields });
     newItem = await newItem.save();
 
-    // const item = await db.Item.findByPk(newItem.id, {
-    //   include: db.ItemCategory,
-    // });
     const item = await db.Item.findById(newItem._id).populate("categoryId");
     return item;
   } catch (err) {
@@ -107,7 +56,6 @@ async function createNewitem(itemFields) {
 }
 
 async function updateItem(id, itemFields) {
-  //   await db.Item.update(itemFields, { where: { id } });
   await db.Item.findByIdAndUpdate(id, { $set: { ...itemFields } });
 }
 
