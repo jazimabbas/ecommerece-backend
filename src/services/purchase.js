e16f53920d6e681a77e2eca0e2b9873be9dfabc3
Hassan <hassan@gmail.com>
2022-04-11 12:08:09 +0500

    removed sequelize services

--- src/services/purchase.js
+++
@@ -1,7 +0,0 @@
-const db = require("../models");
-
-async function getAllPurchases(userId) {
-  return db.Purchase.findAll({ where: { userId }, include: db.Shop });
-}
-
-module.exports = { getAllPurchases };
