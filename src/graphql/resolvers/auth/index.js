6f3b976c147b91c29fa79599bc553617febb2f21
Jazim Abbas <jazimabbas87@gmail.com>
2022-05-14 08:16:29 +0500

    registered user

--- src/graphql/resolvers/auth/index.js
+++ src/graphql/resolvers/auth/index.js
@@ -1,3 +1,10 @@
-exports.register = async function () {
-  return { message: "auth register" };
+const authService = require("../../../services/mogno/auth");
+
+exports.register = async function (userPayload) {
+  try {
+    await authService.saveUser(userPayload);
+    return { message: "Successfully registered user" };
+  } catch (err) {
+    throw err;
+  }
 };
