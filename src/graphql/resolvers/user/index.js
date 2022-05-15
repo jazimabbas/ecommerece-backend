4df7a9538ed165ce7fa6fe4017f91d96cb1d0907
Jazim Abbas <jazimabbas87@gmail.com>
2022-05-15 10:05:33 +0500

    update user profile

--- src/graphql/resolvers/user/index.js
+++ src/graphql/resolvers/user/index.js
@@ -1,6 +1,19 @@
+const { combineResolvers } = require("graphql-resolvers");
+const isAuth = require("../../middlewares/is-auth");
+const userService = require("../../../services/mogno/user");
 
-
-exports.updateProfile = async function (userPayload) {
-    console.log("user payload: ", userPayload)
-    return { message: "Update profile"}
-}
+exports.updateProfile = combineResolvers(
+  isAuth,
+  async function (userPayload, args) {
+    const { user: userData } = userPayload;
+    const userFields = {};
+    for (let key in userData) {
+      if (userData[key]) {
+        userFields[key] = userData[key];
+      }
+    }
+    const user = await userService.updateProfile(args._auth.id, userFields);
+    const userResponse = { user, id: user._id };
+    return { message: "Update profile", data: JSON.stringify(userResponse) };
+  }
+);
